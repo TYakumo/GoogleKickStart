a575484b46e9f59a3a648ab33f99f940b21b80c5
Lin Huan Po <foxs91092@gmail.com>
2020-10-08 22:54:25 +0800

    Wrapped Cartesian Tree as a template

--- 2020/RoundD/D.cpp
+++ 2020/RoundD/D.cpp
@@ -30,136 +30,149 @@ struct TreeNode {
         rightChild = nullptr;
         parent = vector <TreeNode*>(MAXLEVEL, nullptr);
     }
+
+    static int getTreeSize(TreeNode* node) {
+        return (node == nullptr) ? 0 : node->treeSize;
+    }
 };
 
-void treeTraversal(TreeNode* root, int precalLevel) {
-    root->depth = 0;
-    root->treeSize = 0;
-    deque <TreeNode*> que;
-    que.push_back(root);
+class CartesianTree {
+    TreeNode* root;
+    unordered_map<int, TreeNode*> idxMap;
 
-    for (int i = 0; i < que.size(); ++i) {
-        TreeNode* now = que[i];
+    void getIdxMapping(TreeNode* root, unordered_map<int, TreeNode*>& idxMap) {
+        deque <TreeNode*> que;
+        que.push_back(root);
 
-        TreeNode* p = now->parent[precalLevel-1];
-        if (p != nullptr) {
-            now->parent[precalLevel] = p->parent[precalLevel-1];
+        while (!que.empty()) {
+            TreeNode* now = que.front();
+            que.pop_front();
+
+            idxMap[now->idx] = now;
+
+            if (now->leftChild) {
+                que.push_back(now->leftChild);
+            }
+
+            if (now->rightChild) {
+                que.push_back(now->rightChild);
+            }
+        }
+    }
+
+    void treeTraversal(TreeNode* root, int precalLevel) {
+        root->depth = 0;
+        root->treeSize = 0;
+        deque <TreeNode*> que;
+        que.push_back(root);
+
+        for (int i = 0; i < que.size(); ++i) {
+            TreeNode* now = que[i];
+
+            TreeNode* p = now->parent[precalLevel-1];
+            if (p != nullptr) {
+                now->parent[precalLevel] = p->parent[precalLevel-1];
+            }
+
+            if (now->leftChild != nullptr) {
+                now->leftChild->depth = now->depth+1;
+                que.push_back(now->leftChild);
+            }
+
+            if (now->rightChild != nullptr) {
+                now->rightChild->depth = now->depth+1;
+                que.push_back(now->rightChild);
+            }
         }
 
-        if (now->leftChild != nullptr) {
-            now->leftChild->depth = now->depth+1;
-            que.push_back(now->leftChild);
-        }
+        while (!que.empty()) {
+            TreeNode* now = que.back();
+            que.pop_back();
 
-        if (now->rightChild != nullptr) {
-            now->rightChild->depth = now->depth+1;
-            que.push_back(now->rightChild);
+            now->treeSize = 1;
+            if (now->leftChild != nullptr) {
+                now->treeSize += now->leftChild->treeSize;
+            }
+
+            if (now->rightChild != nullptr) {
+                now->treeSize += now->rightChild->treeSize;
+            }
         }
     }
 
-    while (!que.empty()) {
-        TreeNode* now = que.back();
-        que.pop_back();
-
-        now->treeSize = 1;
-        if (now->leftChild != nullptr) {
-            now->treeSize += now->leftChild->treeSize;
-        }
-
-        if (now->rightChild != nullptr) {
-            now->treeSize += now->rightChild->treeSize;
+    void precalculationLCA(TreeNode* root) {
+        // precalculation
+        for (int l = 1; l < MAXLEVEL; ++l) {
+            treeTraversal(root, l);
         }
     }
-}
 
-void precalculationLCA(TreeNode* root) {
-    // precalculation
-    for (int l = 1; l < MAXLEVEL; ++l) {
-        treeTraversal(root, l);
-    }
-}
+    TreeNode* __buildCartesianTree(VI& seq) {
+        vector <TreeNode*> stk;
+        auto cmp = [](int v1, int v2) { return v1 > v2; }; // customize this if type or order needs to be changed
 
-TreeNode* __buildCartesianTree(VI& seq) {
-    vector <TreeNode*> stk;
-    auto cmp = [](int v1, int v2) { return v1 > v2; }; // customize this if type or order needs to be changed
+        for (int i = 0; i < seq.size(); ++i) {
+            TreeNode* now = new TreeNode(seq[i], i);
 
-    for (int i = 0; i < seq.size(); ++i) {
-        TreeNode* now = new TreeNode(seq[i], i);
+            // cout << "SEQ " << seq[i] << endl;
+            if (stk.empty() || cmp(stk.back()->val, seq[i])) {
+                stk.push_back(now);
+            } else {
+                // clearing the stack
+                TreeNode* topNode = nullptr;
 
-        // cout << "SEQ " << seq[i] << endl;
-        if (stk.empty() || cmp(stk.back()->val, seq[i])) {
-            stk.push_back(now);
-        } else {
-            // clearing the stack
+                while (!stk.empty() && !cmp(stk.back()->val, seq[i])) {
+                    stk.back()->rightChild = topNode;
+                    if (topNode != nullptr) {
+                        topNode->parent[0] = stk.back();
+                    }
+
+                    topNode = stk.back();
+                    stk.pop_back();
+                }
+
+                now->leftChild = topNode;
+                if (topNode != nullptr) {
+                    topNode->parent[0] = now;
+                }
+                stk.push_back(now);
+            }
+        }
+
+        if (stk.size() > 1) {
             TreeNode* topNode = nullptr;
 
-            while (!stk.empty() && !cmp(stk.back()->val, seq[i])) {
+            while (!stk.empty()) {
                 stk.back()->rightChild = topNode;
                 if (topNode != nullptr) {
                     topNode->parent[0] = stk.back();
                 }
-
                 topNode = stk.back();
                 stk.pop_back();
             }
 
-            now->leftChild = topNode;
-            if (topNode != nullptr) {
-                topNode->parent[0] = now;
-            }
-            stk.push_back(now);
+            return topNode;
         }
+
+        return stk.back();
+    }
+public:
+    static CartesianTree* buildCartesianTree(vector<int>& seq) {
+        CartesianTree* ctree = new CartesianTree();
+        ctree->root = ctree->__buildCartesianTree(seq);
+        ctree->precalculationLCA(ctree->root);
+        ctree->getIdxMapping(ctree->root, ctree->idxMap);
+        return ctree;;
     }
 
-    if (stk.size() > 1) {
-        TreeNode* topNode = nullptr;
-
-        while (!stk.empty()) {
-            stk.back()->rightChild = topNode;
-            if (topNode != nullptr) {
-                topNode->parent[0] = stk.back();
-            }
-            topNode = stk.back();
-            stk.pop_back();
-        }
-
-        return topNode;
+    TreeNode* getTreeNodeByIdx(int idx) {
+        return idxMap[idx];
     }
 
-    return stk.back();
-}
-
-TreeNode* buildCartesianTree(vector<int>& seq) {
-    TreeNode* root = __buildCartesianTree(seq);
-    // cerr << "TREE BUILT" << endl;
-    precalculationLCA(root);
-    // cerr << "LCA BUILT" << endl;
-    return root;
-}
-
-inline int getTreeSize(TreeNode* node) {
-    return (node == nullptr) ? 0 : node->treeSize;
-}
-
-void getIdxMapping(TreeNode* root, unordered_map<int, TreeNode*>& idxMap) {
-    deque <TreeNode*> que;
-    que.push_back(root);
-
-    while (!que.empty()) {
-        TreeNode* now = que.front();
-        que.pop_front();
-
-        idxMap[now->idx] = now;
-
-        if (now->leftChild) {
-            que.push_back(now->leftChild);
-        }
-
-        if (now->rightChild) {
-            que.push_back(now->rightChild);
-        }
+    TreeNode* getRoot() {
+        return root;
     }
-}
+};
 
 int main()
 {
@@ -179,16 +192,14 @@ int main()
         }
 
         cout << "Case #" << tc << ":";
-        TreeNode* root = buildCartesianTree(b);
-        unordered_map<int, TreeNode*> idxMap;
-        getIdxMapping(root, idxMap);
+        CartesianTree* ctree = CartesianTree::buildCartesianTree(b);
 
         for (int q = 0; q < Q; ++q) {
             int K;
             int S;
             cin >> S >> K;
 
-            TreeNode* node = idxMap[--S];
+            TreeNode* node = ctree->getTreeNodeByIdx(--S);
             --K;
 
             if (node == nullptr) { //S == N-1
@@ -201,17 +212,17 @@ int main()
 
             if (node->treeSize >= K) {
                 if (startIdx == S) { // from left
-                    if (K <= getTreeSize(node->leftChild)) { // to left
+                    if (K <= TreeNode::getTreeSize(node->leftChild)) { // to left
                         ans = S - K;
                     } else { // to right
-                        ans = S + (K - getTreeSize(node->leftChild));
+                        ans = S + (K - TreeNode::getTreeSize(node->leftChild));
                     }
                 }
                 // } else { // from right, startIdx == S+1
-                //     if (K <= getTreeSize(node->rightChild)) { // to right
+                //     if (K <= TreeNode::getTreeSize(node->rightChild)) { // to right
                 //         ans = S + K;
                 //     } else { // to left
-                //         ans = S - (K - getTreeSize(node->rightChild));
+                //         ans = S - (K - TreeNode::getTreeSize(node->rightChild));
                 //     }
                 // }
             } else { // find a node->treeSize >= K
@@ -227,9 +238,9 @@ int main()
                 }
 
                 if (S > node->idx) { // from right to left
-                    ans = node->idx + 1 - (K - getTreeSize(node->rightChild));
+                    ans = node->idx + 1 - (K - TreeNode::getTreeSize(node->rightChild));
                 } else { // left to right
-                    ans = node->idx + (K - getTreeSize(node->leftChild));
+                    ans = node->idx + (K - TreeNode::getTreeSize(node->leftChild));
                 }
             }
 
