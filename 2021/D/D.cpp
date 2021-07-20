6b72d30a034bc483ab972cfa9f06f864ce81b0aa
Lin Huan Po <foxs91092@gmail.com>
2021-07-20 23:09:26 +0900

    2021 D problem D up-solved

--- 2021/D/D.cpp
+++ 2021/D/D.cpp
@@ -60,8 +60,14 @@ public:
     }
 };
 
-using VFT = vector <FenwickTree<long long>>;
-using VVFT = vector <VFT>;
+inline long long powCnt(long long val, long long P) {
+    long long ret = 0;
+    while (val%P == 0 && val != 0) {
+        ++ret;
+        val /= P;
+    }
+    return ret;
+}
 
 int main()
 {
@@ -75,27 +81,28 @@ int main()
         long long P;
         cin >> N >> Q >> P;
 
-        VLL A(N);
-        VFT precal(4, FenwickTree<long long>(N+5));
+        FenwickTree<long long> ft(N+5);
+        FenwickTree<long long> fts(N+5);
+        FenwickTree<long long> ftd(N+5);
+        FenwickTree<long long> ftdc(N+5);
 
         for (int i = 0; i < N; ++i) {
-            cin >> A[i];
+            long long val;
+            cin >> val;
 
-            long long MP = A[i]%P;
-            long long vval = A[i];
-            long long mval = MP;
+            long long MP = val%P;
+            if (MP == val) {
+                ftdc.addVal(i+1, 1);
+                continue;
+            }
 
-            for (int k = 0; k < 4; ++k) {
-                long long diff = vval - mval;
-                int cnt = 0;
-
-                while (diff%P == 0 && diff) {
-                    diff /= P;
-                    ++cnt;
-                }
-                precal[k].addVal(i+1, cnt);
-                vval *= A[i];
-                mval *= MP;
+            if (MP != 0) {
+                long long diff = val-MP;
+                ft.addVal(i+1, powCnt(diff, P));
+                fts.addVal(i+1, powCnt(diff, P) + powCnt(val+MP, P) - 1);
+            } else {
+                ftd.addVal(i+1, powCnt(val, P));
+                ftdc.addVal(i+1, 1);
             }
         }
 
@@ -108,31 +115,40 @@ int main()
                 long long pos;
                 long long val;
                 cin >> pos >> val;
-                A[pos-1] = val;
                 long long MP = val%P;
-                long long vval = val;
-                long long mval = MP;
+                ft.addVal(pos, -ft.queryRange(pos, pos));
+                fts.addVal(pos, -fts.queryRange(pos, pos));
+                ftd.addVal(pos, -ftd.queryRange(pos, pos));
+                ftdc.addVal(pos, -ftdc.queryRange(pos, pos));
 
-                for (int k = 0; k < 4; ++k) {
-                    long long diff = vval - mval;
-                    int cnt = 0;
+                if (MP == val) {
+                    ftdc.addVal(pos, 1);
+                    continue;
+                }
 
-                    while (diff%P == 0 && diff) {
-                        diff /= P;
-                        ++cnt;
-                    }
-
-                    precal[k].addVal(pos, -precal[k].queryRange(pos, pos));
-                    precal[k].addVal(pos, cnt);
-                    vval *= val;
-                    mval *= MP;
+                if (MP != 0) {
+                    long long diff = val-MP;
+                    ft.addVal(pos, powCnt(diff, P));
+                    fts.addVal(pos, powCnt(diff, P) + powCnt(val+MP, P) - 1);
+                } else {
+                    ftd.addVal(pos, powCnt(val, P));
+                    ftdc.addVal(pos, 1);
                 }
             } else {
                 long long S;
                 long long L;
                 long long R;
                 cin >> S >> L >> R;
-                cout << " " << precal[S-1].queryRange(L, R);
+
+                long long len = R-L+1;
+                long long ans = ftd.queryRange(L, R)*S + (len - ftdc.queryRange(L, R))*powCnt(S, P);
+                if (P == 2 && S%2 == 0) {
+                    ans += fts.queryRange(L, R);
+                } else {
+                    ans += ft.queryRange(L, R);
+                }
+
+                cout << " " << ans;
             }
         }
         cout << endl;
